2c548ddf217637ad44f86306f0555e33ba9a0cbd
gitpBansal031 <bansalpulkit3103@gmail.com>
2024-10-05 22:22:38 +0530

    added mark as done feature

--- Todo List/src/items.jsx
+++ Todo List/src/items.jsx
@@ -7,11 +7,19 @@ function Items() {
         newval(event.target.value);
     }
     function add() {
-        settasks([...tasks, { task: val, key: uuidv4() }]);
+        settasks([...tasks, { task: val, key: uuidv4(), done: false }]);
     }
     function del(id) {
-        tasks=tasks.filter((task) => task.key != id );
+        tasks = tasks.filter((task) => task.key != id);
         settasks(tasks);
+    }
+    function check(id) {
+        tasks.map((task) => {
+            if (task.key == id) {
+                task.done = task.done ? false : true;
+                settasks([...tasks]);
+            }
+        });
     }
     return (
         <div>
@@ -23,7 +31,13 @@ function Items() {
             <ul>
                 {tasks.map((task) => (
                     <div>
-                        <li key={task.key}>{task.task} &nbsp; &nbsp; <button onClick={() => del(task.key)}>Delete</button></li>
+                        <li
+                            key={task.key}>{task.task} &nbsp; &nbsp; <button onClick={() => del(task.key)}>Delete</button>
+                            &nbsp;&nbsp;
+                            {task.done ? <button onClick={() => check(task.key)}>Unmark</button> : <button onClick={() => check(task.key)}>Mark Done</button>}
+                            &nbsp;&nbsp;
+                            {task.done ? <span>Completed</span> : null}
+                        </li>
                         <br />
                     </div>
                 ))}
